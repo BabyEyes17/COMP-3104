af88ed5e0c0852dcce7953e06c53ec5a2207ed6c
Jayden Lewis <jaydent.lewis@gmail.com>
2025-10-01 20:12:17 -0400

    React app build, added ID and name

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: StudentID || Student Name || DevOps</h5>
+      <h5>Developed By: 101484621 || Jayden Lewis || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
